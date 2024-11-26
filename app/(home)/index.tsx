3882ed8bce918b19055e4741fc6f09e12a638529
Juan Martin Zabala Ochoa <juanmarzabala8a@gmail.com>
2024-11-26 17:18:53 -0500

    better TextInput

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -1,94 +1,99 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import {
-  StyleSheet,
-  TextInput,
-  View,
-  KeyboardAvoidingView,
-  Keyboard,
-  Platform,
-  ScrollView,
-  Text,
+    StyleSheet,
+    View,
+    KeyboardAvoidingView,
+    Keyboard,
+    Platform,
+    Text,
 } from "react-native";
+import { GestureHandlerRootView, PanGestureHandler, PanGestureHandlerGestureEvent, TextInput } from "react-native-gesture-handler";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function Home() {
-  const [text, setText] = useState("");
-  const insets = useSafeAreaInsets();
+    const [text, setText] = useState("");
+    const insets = useSafeAreaInsets();
+    const textInputRef = useRef(null);
+    const [isSliding, setIsSliding] = useState(false);
 
-  const dismissKeyboard = () => {
-    Keyboard.dismiss();
-  };
+    const dismissKeyboard = () => {
+        Keyboard.dismiss();
+    };
 
-  return (
-    <KeyboardAvoidingView
-      style={styles.container}
-      behavior={Platform.OS === "ios" ? "padding" : undefined}
-    >
+    const handleGestureEvent = (event: PanGestureHandlerGestureEvent) => {
+        const { translationY, translationX } = event.nativeEvent;
+        setIsSliding(translationX > 0 || translationY > 0)
+    };
+
+    const handleGestureEnd = () => {
+        setIsSliding(false);
+    };
+
+    return (
+        <KeyboardAvoidingView
+            style={styles.container}
+            behavior={Platform.OS === "ios" ? "padding" : undefined}
+        >
         <View style={[styles.header, {height: 60 + insets.top, paddingTop: insets.top}]}>
             <Text style={styles.titleText}>
-                {"Sapo"}
+                {"S.A.P.O"}
             </Text>
         </View>
-        <View style={styles.innerContainer}>
-          <TextInput
-            style={styles.textInput}
-            multiline
-            value={text}
-            // onChangeText={handleTextChange}
-            onChangeText={setText}
-            scrollEnabled={true}
-            
-            // scrollEnabled={false}
-            placeholder="Type something..."
-            placeholderTextColor="#aaa"
-            returnKeyType="done"
-            submitBehavior="blurAndSubmit" 
-            onSubmitEditing={dismissKeyboard}
-            // onContentSizeChange={handleContentSizeChange}
-          />
-        </View>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <PanGestureHandler 
+                onGestureEvent={handleGestureEvent}
+                onEnded={handleGestureEnd}
+                simultaneousHandlers={textInputRef}
+            >
+                <View style={styles.innerContainer}>
+                    <TextInput
+                        ref={textInputRef}
+                        style={[styles.textInput, {height: '100%'}]}
+                        multiline
+                        value={text}
+                        onChangeText={setText}
+                        scrollEnabled={true}
+                        placeholder="Type something..."
+                        placeholderTextColor="#aaa"
+                        returnKeyType="done"
+                        submitBehavior="blurAndSubmit" 
+                        onSubmitEditing={dismissKeyboard}
+                        editable={!isSliding}
+                    />
+                </View>
+            </PanGestureHandler>
+        </GestureHandlerRootView>
     </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
-  titleText: {
-    fontSize: 20,
-    // backgroundColor: "blue",
-    textAlign: 'center',
-    fontWeight: 'bold',
-  },
-  scrollContent: {
-    flexGrow: 1, // Ensures scrolling even with small content
-  },
-  header: {
-    width: '100%',
-    // backgroundColor: "red",
-    // paddingVertical: 30,
-    justifyContent: 'center'
-  },
-  innerContainer: {
-    flex: 1,
-    // paddingVertical: 30,
-    paddingHorizontal: 15,
-    justifyContent: "flex-start", // TextInput starts at the top
-    // alignItems: "flex-end", // Align to the right
-  },
-  textInput: {
-    fontSize: 36, // Big text
-    lineHeight: 36, // Space between lines
-    textAlign: "left", // Align text to the right
-    textAlignVertical: "top", // Align text to the top
-    // borderColor: "#ddd", // Optional border
-    // borderWidth: 1,
-    // borderRadius: 8,
-    padding: 10,
-    width: "100%", // Full width
-    backgroundColor: "#fff", // Background color
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+    },
+    titleText: {
+        fontSize: 20,
+        textAlign: 'center',
+        fontWeight: 'bold',
+    },
+    header: {
+        width: '100%',
+        justifyContent: 'center'
+    },
+    innerContainer: {
+        flex: 1,
+        justifyContent: "flex-start",
+    },
+    textInput: {
+        fontSize: 36,
+        lineHeight: 36,
+        textAlign: "left",
+        textAlignVertical: "top",
+        paddingHorizontal: 25,
+        paddingVertical: 10,
+        width: "100%",
+        backgroundColor: "#fff",
+    },
 });
 
