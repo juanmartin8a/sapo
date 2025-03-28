b2049cc7eaeae324b85bf0e715e18a870e585a7a
Juan Martin Zabala Ochoa <juanmarzabala8a@gmail.com>
2025-03-28 12:10:25 -0500

    constants

---
+++ constants/languages.ts
@@ -0,0 +1,16 @@
+export const languages = {
+    1: "English",
+    2: "Spanish",
+    3: "Portuguese",
+    4: "French",
+    5: "German",
+    6: "Arabic",
+    7: "Mandarin (Standard Chinese)",
+    8: "Russian",
+} 
+
+
+export const languagesPlusAutoDetect = {
+    ...languages,
+    9: "Auto-detect"
+} 
