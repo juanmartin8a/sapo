3c5046a77e54381031c5fe413307d5c54c09b778
Juan Martin Zabala Ochoa <juanmarzabala8a@gmail.com>
2025-12-11 18:43:04 -0500

    ...

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,4 @@
+import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { Stack } from "expo-router";
 import { ClerkProvider } from '@clerk/clerk-expo';
 import { tokenCache } from '@clerk/clerk-expo/token-cache';
@@ -6,12 +7,17 @@ import { KeyboardProvider } from "react-native-keyboard-controller";
 
 WebBrowser.maybeCompleteAuthSession();
 
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
+    unsavedChangesWarning: false,
+});
+
 export default function RootLayout() {
     return (
-        <ClerkProvider tokenCache={tokenCache}>
+        // <ClerkProvider tokenCache={tokenCache}>
+        <ConvexProvider client={convex}>
             <KeyboardProvider>
                 <Stack screenOptions={{ headerShown: false }} />
             </KeyboardProvider>
-        </ClerkProvider>
+        </ConvexProvider>
     );
 }
