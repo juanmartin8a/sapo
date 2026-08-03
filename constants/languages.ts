80803ae1e6da790097cfc89988a584b8f71631e5
Juan Martin Zabala Ochoa <juanmarzabala8a@gmail.com>
2026-08-03 15:28:20 -0500

    Added new languages and changed their order

--- constants/languages.ts
+++ constants/languages.ts
@@ -4,15 +4,25 @@ export const DEFAULT_TARGET_LANGUAGE_ID = 1;
 export const AUTO_DETECT_LANGUAGE_LABEL = "Auto-detect";
 
 export const languages = {
-    1: "English",
-    2: "Spanish",
-    3: "French",
-    4: "German",
-    5: "Russian",
-    6: "Arabic",
-    7: "Mandarin (Standard Chinese)",
-    8: "Japanese",
-}
+  1: "English",
+  2: "Spanish",
+  3: "French",
+  4: "Portuguese",
+  5: "Mandarin Chinese",
+  6: "Japanese",
+  7: "Arabic",
+  8: "German",
+  9: "Korean",
+  10: "Hindi",
+  11: "Italian",
+  12: "Russian",
+  13: "Urdu",
+  14: "Indonesian",
+  15: "Turkish",
+  16: "Swahili",
+  17: "Polish",
+  18: "Greek",
+} as const;
 
 
 export const languagesPlusAutoDetect = {
